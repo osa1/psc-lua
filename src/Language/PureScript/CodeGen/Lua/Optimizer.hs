0f04f774043dc5989490ac069a34ce5493537906
osa1 <omeragacan@gmail.com>
2014-05-09 17:59:26 +0300

    start implementing inlining (does not preserve semantics because of pretty-printer problems)

--- src/Language/PureScript/CodeGen/Lua/Optimizer.hs
+++ src/Language/PureScript/CodeGen/Lua/Optimizer.hs
@@ -7,9 +7,11 @@ import qualified Language.Lua.Syntax as L
 import Language.PureScript.CodeGen.Lua.Utils
 
 import Data.Generics
+import Data.List (foldl')
+import Prelude hiding (exp)
 
 optimize :: L.Block -> L.Block
-optimize = removeIfTrues . removeDollars
+optimize = inlineCommonOperators . removeIfTrues . removeDollars
 
 -- | Replace `Prelude.$` calls with direct function applications.
 -- This assumes using standard Prelude.
@@ -22,7 +24,7 @@ removeDollars = everywhere (mkT removeDollarExp)
                                       (L.Args [arg1])))
         (L.Args [arg2])))) =
       L.PrefixExp $ L.PEFunCall $ L.NormalFunCall (expToPexp arg1) $ L.Args [arg2]
-    removeDollarExp e = e
+    removeDollarExp exp = exp
 
 -- | Replace `if true then stats end` with `stats`. If `stats` has a return
 -- statement, then remove all following statements.
@@ -48,4 +50,49 @@ removeIfTrues = everywhere (mkT removeIfTrue)
     searchIfTrue (stat : stats) = do
       (prevs, ifBody, rest) <- searchIfTrue stats
       return (stat : prevs, ifBody, rest)
+
+applyAll :: Data a => [a -> a] -> a -> a
+applyAll = foldl' (.) id
+
+inlineCommonOperators :: Data a => a -> a
+inlineCommonOperators = everywhere (mkT inlineCommonOperators')
+
+inlineCommonOperators' :: L.Exp -> L.Exp
+inlineCommonOperators' = applyAll
+    [ binary "numNumber" "_plus" L.Add
+    , binary "numNumber" "_minus" L.Sub
+    , binary "numNumber" "_times" L.Mul
+    , binary "numNumber" "_div" L.Div
+    , binary "numNumber" "_percent" L.Mod
+
+    , binary "ordNumber" "_less" L.LT
+    , binary "ordNumber" "_greater" L.GT
+
+    , binary "eqNumber" "_eq_eq" L.EQ
+
+    , binary "boolLikeBoolean" "_less" L.LT
+    , binary "boolLikeBoolean" "_amp_amp" L.And
+
+    -- , unary "boolLikeBoolean" "__not" L.Neg
+    ]
+  where
+    binary :: String -> String -> L.Binop -> L.Exp -> L.Exp
+    binary typecls method binop exp =
+      case exp of
+        (L.PrefixExp (L.PEFunCall (L.NormalFunCall
+          (L.PEFunCall (L.NormalFunCall
+            (L.PEFunCall (L.NormalFunCall
+              (L.PEVar (L.Select (L.PEVar (L.VarName "Prelude")) (L.String method')))
+              (L.Args [L.PrefixExp (L.PEFunCall
+                         (L.NormalFunCall
+                           (L.PEVar (L.Select (L.PEVar (L.VarName "Prelude")) (L.String typecls')))
+                           (L.Args [L.TableConst []])))])))
+            (L.Args [arg1])))
+          (L.Args [arg2]))))
+          | method' == method && typecls' == typecls -> L.Binop binop arg1 arg2
+          | otherwise -> exp
+        _ -> exp
+
+    unary :: String -> String -> L.Unop -> L.Exp -> L.Exp
+    unary typecls method unop exp = undefined
 
