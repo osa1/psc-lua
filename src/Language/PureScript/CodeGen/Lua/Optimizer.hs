423b2463895a35416408e1c35593368c048e12c2
osa1 <omeragacan@gmail.com>
2014-05-29 07:26:23 +0300

    generalize reducing optimizations a bit

--- src/Language/PureScript/CodeGen/Lua/Optimizer.hs
+++ src/Language/PureScript/CodeGen/Lua/Optimizer.hs
@@ -12,44 +12,64 @@ import Data.List (foldl')
 import qualified Data.Set as S
 import Prelude hiding (exp)
 
+untilFixpoint :: Eq a => (a -> a) -> a -> a
+untilFixpoint f a =
+    let a' = f a
+    in if a' == a then a else untilFixpoint f a'
+
 applyAll :: [a -> a] -> a -> a
 applyAll = foldl' (.) id
 
 optimize :: L.Block -> L.Block
 optimize = applyAll
-  [applyVars, inlineEffOps, removeReturnApps, removeVarAsgns,
+  [applyVars', applyVars, inlineEffOps, removeReturnApps, removeVarAsgns,
    removeDupAsgns, inlineCommonOperators, removeIfTrues, removeDollars]
 
--- | Reduce statements like
--- `return (function (var1, var2, ...) return function (...) ... end end)(var1', var2', ...)`
--- to
--- `return (function (...) ... end)`
--- by reducing the outer function application. `var1', var2', ...` should be all vars
 applyVars :: Data a => a -> a
 applyVars = everywhere (mkT applyVar)
   where
     applyVar
-      b@(L.Block [] (Just
-          [ L.PrefixExp (L.PEFunCall
-              (L.NormalFunCall
-               (L.Paren (L.EFunDef (L.FunBody args _ block)))
-               (L.Args vars))) ]))
-      | all isVar vars && length args == length vars =
+      (L.Block stats (Just
+        [ L.PrefixExp (L.PEFunCall
+            (L.NormalFunCall
+             (L.Paren (L.EFunDef (L.FunBody args _ block)))
+             (L.Args vars))) ]))
+      | all isVar vars && not (null args) && length args == length vars =
           let vars' = map getVar vars
-          in substBlock' (zip args vars') block
-      | otherwise = b
+              L.Block stats' ret = substBlock' (zip args vars') block
+          in L.Block (stats ++ stats') ret
     applyVar b = b
 
     substBlock' :: [(L.Name, L.Name)] -> L.Block -> L.Block
     substBlock' [] b = b
     substBlock' ((r, l) : ss) b = substBlock' ss (runSubst 0 (substBlock r l b))
 
-    isVar (L.PrefixExp L.PEVar{}) = True
-    isVar _ = False
+applyVars' :: Data a => a -> a
+applyVars' = everywhere (mkT applyVar)
+  where
+    applyVar :: L.PrefixExp -> L.PrefixExp
+    applyVar
+      (L.PEFunCall (L.NormalFunCall
+        (L.Paren (L.EFunDef (L.FunBody args _ (L.Block [] (Just [exp])))))
+        (L.Args vars)))
+      | all isVar vars && not (null args) && length args == length vars =
+          let vars' = map getVar vars
+          in L.Paren (substExp' (zip args vars') exp)
+    applyVar e = e
 
-    getVar :: L.Exp -> L.Name
-    getVar (L.PrefixExp (L.PEVar (L.VarName var))) = var
-    getVar notAVar = error $ "getVar on a non-var: " ++ show notAVar
+    substExp' :: [(L.Name, L.Name)] -> L.Exp -> L.Exp
+    substExp' [] e = e
+    substExp' ((r, l) : ss) e = substExp' ss (runSubst 0 (substExp r l e))
+
+isVar :: L.Exp -> Bool
+isVar (L.PrefixExp L.PEVar{}) = True
+isVar (L.PrefixExp (L.Paren p)) = isVar p
+isVar _ = False
+
+getVar :: L.Exp -> L.Name
+getVar (L.PrefixExp (L.PEVar (L.VarName var))) = var
+getVar (L.PrefixExp (L.Paren e)) = getVar e
+getVar notAVar = error $ "getVar on a non-var: " ++ show notAVar
 
 -- | Replace `Prelude.$` calls with direct function applications.
 -- This assumes using standard Prelude.
@@ -124,11 +144,6 @@ removeVarAsgns = everywhere (mkT rmVarAsgns)
       let (stats', rets') = iterStats rest rets
       in (stat : stats', rets')
     iterStats [] rets = ([], rets)
-
-    isVar :: L.Exp -> Bool
-    isVar (L.PrefixExp L.PEVar{}) = True
-    isVar (L.PrefixExp (L.Paren p)) = isVar p
-    isVar _ = False
 
     subst :: Data a => L.Name -> L.Exp -> a -> a
     subst var rhs = everywhere (mkT subst')
